c1a2696a1f5097bbf63f3ad0eda9ec7aebb0f48b
DescentDD <1740811552@qq.com>
2023-08-01 01:12:49 +0800

    6

--- api/python.py
+++ api/python.py
@@ -1,11 +1,31 @@
 from http.server import BaseHTTPRequestHandler
- 
+import os
+import re
+import random
+
+def find_png_files(directory):
+    file_list = os.listdir(directory)
+    png_files = [file_name for file_name in file_list if re.search(r'\.(png|jpg)$', file_name)]
+    return png_files
  
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
+        imgs=find_png_files('api\img')
+        img = random.choice(imgs)
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        content_type = 'image/png' if img.lower().endswith('.png') else 'image/jpeg'
+        self.send_header('Content-type', content_type)
         self.end_headers()
-        self.wfile.write("DearXuan's API by python!".encode())
+        with open('api/img/'+img, 'rb') as file:
+            content = file.read()
+            #print(content)
+            print(type(content))
+            self.wfile.write(content)
         return
+    
+if __name__ == '__main__':
+    from http.server import HTTPServer
+    server = HTTPServer(('localhost', 8080), handler)
+    print('Starting server at http://localhost:8080')
+    server.serve_forever()
